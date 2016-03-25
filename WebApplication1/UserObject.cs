76d7f120d82b880cee1c7c9d59d0276b4bb36c98
Aleko Spoonamore <aspoonamore93@gmail.com>
2016-03-24 20:00:41 -0500

    Created User and Trainer Objects for Session states

---
+++ WebApplication1/UserObject.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1
+{
+    public class UserObject
+    {
+        private int userId;
+        private string imagePath;
+        private string email;
+        private string phone;
+        private string gender;
+        private string dateOfBirth;
+        private string firstName;
+        private string middleName;
+        private string lastName;
+        private string homeAddress;
+        private string bio;
+
+
+        public UserObject()
+        {
+            userId = 0;
+            imagePath = "";
+            email = "";
+            gender = "";
+            dateOfBirth = "";
+            firstName = "";
+            middleName = "";
+            lastName = "";
+            phone = "";
+            homeAddress = "";
+            bio = "";
+
+        }
+
+        public int UserId
+        {
+            get { return userId; }
+            set { userId = value; }
+        }
+
+        public string ImagePath
+        {
+            get { return imagePath; }
+            set { imagePath = value; }
+        }
+
+        public string Email
+        {
+            get { return email; }
+            set { email = value; }
+        }
+
+        public string Gender
+        {
+            get { return Gender; }
+            set { gender = value; }
+        }
+
+        public string DateOfBirth
+        {
+            get { return dateOfBirth; }
+            set { dateOfBirth = value; }
+        }
+
+        public string FirstName
+        {
+            get { return firstName; }
+            set { firstName = value; }
+        }
+
+        public string MiddleName
+        {
+            get { return middleName; }
+            set { middleName = value; }
+        }
+
+        public string LastName
+        {
+            get { return lastName; }
+            set { lastName = value; }
+        }
+
+        public string HomeAddress
+        {
+            get { return homeAddress; }
+            set { homeAddress = value; }
+        }
+
+        public string Bio
+        {
+            get { return bio; }
+            set { bio = value; }
+        }
+    }
+}
