d9ac25e814dc746b31432ba5799a60ca1e1124b5
Aleko Spoonamore <aspoonamore93@gmail.com>
2016-04-11 14:11:54 -0500

    Save Chenges to Scheduler

--- WebApplication1/TrainerScheduler.aspx.cs
+++ WebApplication1/TrainerScheduler.aspx.cs
@@ -11,7 +11,34 @@ namespace WebApplication1
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            string startDate = Calendar1.TodaysDate.ToShortDateString();
+            TextBox1.Text = startDate;
+            DateTextBox.Text = startDate;
+        }
+
+        protected void Calendar1_SelectionChanged(object sender, EventArgs e)
+        {
+
+            TextBox1.Text = Calendar1.SelectedDate.ToShortDateString();
+            DateTextBox.Text = Calendar1.SelectedDate.ToShortDateString();
 
         }
+
+        protected void Calendar1_DayRender(object sender, DayRenderEventArgs e)
+        { 
+            //If Date is listed on date blocked table, then mark as slate gray
+            //If Date is completly blocked, then mark gray
+            if(e.Day.Date == Convert.ToDateTime("4/12/2016"))
+            {
+                e.Cell.BackColor = System.Drawing.Color.SlateGray;
+            }
+
+            if (e.Day.Date == Convert.ToDateTime("4/13/2016"))
+            {
+                e.Cell.BackColor = System.Drawing.Color.Gray;
+            }
+        }
+
+
     }
 }
