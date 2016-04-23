62b75931c94be7dab432a6aa39b3cf431c1b42a0
Johnpavy <johnpavelchak@yahoo.com>
2016-04-23 12:46:54 -0500

    Made trainer confirmation page url pass the trainer id to retrieve trainer info and populate the trainer object

--- WebApplication1/ConfirmationPage.aspx.cs
+++ WebApplication1/ConfirmationPage.aspx.cs
@@ -12,14 +12,54 @@ namespace WebApplication1
 {
     public partial class ConfirmationPage : System.Web.UI.Page
     {
+        TrainerObject Tobj = new TrainerObject();
+
         protected void Page_Load(object sender, EventArgs e)
         {
+            String userID = Request.QueryString["UserID"];
+            int userIDint = Int32.Parse(userID);
             if (!this.IsPostBack)
             {
                 //SqlConnection trainerDb = new SqlConnection(SqlDataSource1.ConnectionString);
+                if (Session["TrainerInfo"] == null)
+                {
 
-              //  string constr = ConfigurationManager.ConnectionStrings["constr"].ConnectionString;
-                string activationCode = !string.IsNullOrEmpty(Request.QueryString["ActivationCode"]) ? Request.QueryString["ActivationCode"] : Guid.Empty.ToString();
+                    //Forces a redirect to splash page if this page is loaded without a session state.
+                    SqlConnection db = new SqlConnection(SqlDataSource1.ConnectionString);
+                    SqlCommand cmd = new SqlCommand();
+                    cmd.CommandType = System.Data.CommandType.Text;
+                    cmd.Connection = db;
+
+                    try
+                    {
+                        cmd.CommandText = "Select * FROM [MFNTrainerTable] WHERE Trainer_Id = @Id";
+                        cmd.Parameters.AddWithValue("@Id", userIDint);
+                        db.Open();
+
+                        SqlDataReader sdr = cmd.ExecuteReader();
+
+                        while (sdr.Read())
+                        {
+                            Tobj.FirstName = sdr["Trainer_FirstName"].ToString();
+                            Tobj.LastName = sdr["Trainer_LastName"].ToString();
+                            Tobj.Email = sdr["Trainer_Email"].ToString();
+
+                        }
+
+                        Session["TrainerInfo"] = Tobj;
+
+
+                    }
+                    catch
+                    {
+                        Response.Redirect("Default.aspx");
+                    }
+
+                }
+                //  string constr = ConfigurationManager.ConnectionStrings["constr"].ConnectionString;
+                String activationCode = !string.IsNullOrEmpty(Request.QueryString["ActivationCode"]) ? Request.QueryString["ActivationCode"] : Guid.Empty.ToString();
+                String UserID = Request.QueryString["UserID"];
+
                 using (SqlConnection con = new SqlConnection(SqlDataSource1.ConnectionString))
                 {
                     //using (SqlCommand cmd = new SqlCommand("SELECT COUNT (*) FROM UserActivation WHERE User_ActivationCode = @ActivationCode"))
@@ -33,8 +73,6 @@ namespace WebApplication1
                             cmd.Connection = con;
                             con.Open();
                             int rowsAffected = cmd.ExecuteNonQuery();
-                           // cmd.CommandText = "DELETE FROM UserActivation WHERE User_ActivationCode = @ActivationCode";
-                            // cmd.ExecuteNonQuery();
                             con.Close();
                             if (rowsAffected == 1)
                             {
