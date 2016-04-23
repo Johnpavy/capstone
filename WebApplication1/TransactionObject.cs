1baa083ccf4ccc45c4429adb04d1ae1ed29afe58
Aleko Spoonamore <aspoonamore93@gmail.com>
2016-04-23 18:34:31 -0500

    Created TransactionObject

---
+++ WebApplication1/TransactionObject.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1
+{
+    public class TransactionObject
+    {
+        private string individualCost;
+        private string additonalPersonCost;
+        private string numberAttending;
+
+        public TransactionObject()
+        {
+        individualCost ="0.00";
+        additonalPersonCost = "0.00";
+        numberAttending = "1";
+        }
+
+        public string IndividualCost
+        {
+            get { return individualCost; }
+            set { individualCost = value; }
+        }
+
+        public string AdditionalPersonCost
+        {
+            get { return additonalPersonCost; }
+            set { additonalPersonCost = value; }
+        }
+
+        public string NumberAttending
+        {
+            get { return numberAttending; }
+            set { numberAttending = value; }
+        }
+    }
+}
